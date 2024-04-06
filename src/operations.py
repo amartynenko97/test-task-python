f2a52bafb97661b0350a403050e790dc0d9258d5
amartynenko97 <antonmartynenko97@gmail.com>
2024-04-06 07:49:44 -0500

    update, add create new user session after update data

--- src/operations.py
+++ src/operations.py
@@ -26,15 +26,15 @@ def test_create_user():
     
     assert response.status_code == 200, f"Failed to create user: {response.text}"
     
-    # Assertions updated user data
-    return positive_assert(response_data["User-Token"], response_data["login"], new_email)
+    return response_data["User-Token"], response_data["login"], new_email, new_password
+   
 
 def positive_assert(user_token, user_login, user_email):
     url = f"{BASE_URL}/users/{user_login}"
     headers = {"Authorization": f"Token token=\"{API_KEY}\"", "User-Token": user_token}
     response = requests.get(url, headers=headers)
     response_data = response.json()
-
+    
     assert response.status_code == 200, f"Failed to retrieve user data: {response.text}"
 
     api_login_field = response_data["login"]
@@ -45,7 +45,7 @@ def positive_assert(user_token, user_login, user_email):
 
     return user_token, api_login_field, api_email_field
 
-def test_update_user(user_token, user_login, user_email):
+def test_update_user(user_token, user_login):
     updated_login = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
     updated_email = f"{updated_login}@example.com"
 
@@ -63,9 +63,24 @@ def test_update_user(user_token, user_login, user_email):
 
     assert response.status_code == 200, f"Failed to update user: {response.text}"
     assert response.json()["message"] == "User successfully updated."
+    return updated_login, updated_email
+    
 
-    # Assertions updated user data
-    positive_assert(user_token, updated_login, updated_email)
+def create_new_user_session(login, password):
+    url = f"{BASE_URL}/api/session"
+    user_data = {
+        "user": {
+            "login": login,
+            "password": password
+        }
+    }
 
-
+    response = requests.post(url, json=user_data)
+    print(response.json())
+    if response.status_code == 200:
+        response_data = response.json()
+        return response_data["User-Token"]
+    else:
+        print(f"Failed to create session: {response.text}")
+        return None
 
--- tests/test_user_operations.py
+++ tests/test_user_operations.py
@@ -3,20 +3,22 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
                 
 import pytest
-from operations import test_create_user, test_update_user
+from operations import test_create_user, positive_assert, test_update_user, create_new_user_session
 
 @pytest.fixture(scope="module")
 def created_user_data():
-    user_token, login, email = test_create_user()
-    yield user_token, login, email
+    user_token, login, email, password = test_create_user()
+    yield user_token, login, email, password
 
+def test_create_users(created_user_data):
+    user_token, login, email, password = created_user_data
+    positive_assert(user_token, login, email)
 
-def test_create_user(created_user_data):
-    user_token, login, email = created_user_data
-    assert user_token is not None
-    assert login is not None
-    assert email is not None
+def test_update_users(created_user_data):
+    user_token, login, email, password = created_user_data
+    
+    updated_login, updated_email = test_update_user(user_token, login)
+    
+    new_user_token = create_new_user_session(updated_email, password)
 
-def test_update_user(created_user_data):
-    user_token, login, email = created_user_data
-    test_update_user(user_token, login, email)
+    positive_assert(new_user_token, updated_login, updated_email)
